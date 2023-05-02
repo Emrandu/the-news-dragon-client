5f575e59e7f887332a5c74c4be4158a324db7581
emran-hossain <swadhinatadu@gmail.com>
2023-05-03 02:52:43 +0600

    hide firebase config

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -8,15 +8,19 @@ export const AuthContext = createContext(null);
 const auth = getAuth(app);
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState (null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) =>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password)
     } 
 
     const signIn = (email, password) =>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password)
     }
     const logOut = () =>{
+        setLoading(true);
         return signOut(auth);
     }
 
@@ -24,6 +28,7 @@ const AuthProvider = ({children}) => {
          const unsubscribe = onAuthStateChanged(auth, loggedUser =>{
             console.log('logged in user inside auth state observer', loggedUser)
             setUser(loggedUser);
+            setLoading(false);
         })
         return () =>{
             unsubscribe();
@@ -32,6 +37,7 @@ const AuthProvider = ({children}) => {
 
     const authInfo = {
         user,
+        loading,
         createUser,
         signIn,
         logOut
