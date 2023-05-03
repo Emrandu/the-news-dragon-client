27e58f285299c048f33c86408eaa3b9229b9b6a8
emran-hossain <swadhinatadu@gmail.com>
2023-05-04 01:04:03 +0600

    finish

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+                loader: ({params}) => fetch(`https://news-dragon-server-emrandu.vercel.app/categories/${params.id}`)
             },
         ]
     },
@@ -52,7 +52,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch(`https://news-dragon-server-emrandu.vercel.app/news/${params.id}`)
             }
         ]
     }
